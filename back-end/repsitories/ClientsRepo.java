7c547e7f8f769cfb040d229e7bd169c457177ba3
muriman13 <100300225+muriman13@users.noreply.github.com>
2022-05-11 00:33:03 +0300

    Delete back-end/repsitories directory

--- back-end/repsitories/ClientsRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repsitories;
-
-import com.example.demo.entities.Clients;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientsRepo extends CrudRepository<Clients,Integer> {
-}
